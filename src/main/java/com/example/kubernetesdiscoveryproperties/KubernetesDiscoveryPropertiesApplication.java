44b1d51a6baa36c6dc6e98b8bf359a150e8630ed
Jan Marten <kontakt@janmarten.dev>
2023-03-30 08:30:25 +0200

    Initial commit

---
+++ src/main/java/com/example/kubernetesdiscoveryproperties/KubernetesDiscoveryPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubernetesdiscoveryproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDiscoveryPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesDiscoveryPropertiesApplication.class, args);
+    }
+
+}
